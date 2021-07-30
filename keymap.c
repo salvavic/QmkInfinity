b26144afb67fb2b13eccbd24f900ddf2f4bb71fb
Salvador Vicente de Valenzuela <salvador.vicente@qwi-ti.com>
2021-07-30 11:38:11 +0200

    Elimino el td para :

--- keymap.c
+++ keymap.c
@@ -193,7 +193,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(_NUMERICO), TD(TD_6), TD(TD_7), TD(TD_8), TD(TD_9),TD(TD_10), TD(TD_11),
         TG(_MOUSE), KC_Y, KC_U, KC_I, KC_O, KC_P, TD(TD_12),
                  KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),  KC_QUOT,
-        TG(_FN), KC_N, KC_M, TD(TD_PC), TD(TD_DOT),  KC_SLSH,  KC_RSFT,
+        TG(_FN), KC_N, KC_M, TD(TD_PC), KC_DOT,  KC_SLSH,  KC_RSFT,
                             TT(_SIMBOLOS), TT(_FN), TG(_MOUSE), KC_RALT, KC_RCTRL,
                                     KC_RALT, KC_RCTRL,
         KC_INS,
